f478ffa5bbaa5208ca34b0e89ae5f6bee9afd28c
Saddam <$ git config --global user.email saddam.shaikh@coditas.com>
2021-07-19 11:22:04 +0530

    few more lines

--- practice.py
+++ practice.py
@@ -5,3 +5,8 @@ print(a+b)
 
 input = input("Enter your name")
 print(a + input)
+print("Welcome " + input)
+list  = ["Its ", "a", "great", "pleasure", "that", "you", "are", 'here']
+
+for item in list :
+    print(item)
