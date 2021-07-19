ff128e671a3bdabeae0b59428d42889568dfd11e
Saddam <$ git config --global user.email saddam.shaikh@coditas.com>
2021-07-19 13:53:56 +0530

    new changes

--- practice.py
+++ practice.py
@@ -8,12 +8,18 @@ print(a + input)
 print("Welcome " + input)
 print("to the " + b + "of python")
 list  = ["Its ", "a", "great", "pleasure", "that", "you", "are", 'here']
+list1 = ["New", "changes", 'needed']
 
 for item in list :
     print(item)
     print("---")
     
+for i in list1:
+        print(i)
+        print("--")
+        
+        
 print("List is over")
 print("lets try git")
 print("new clone")
-
+print(10+11)
